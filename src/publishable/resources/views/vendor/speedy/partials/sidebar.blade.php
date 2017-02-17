be8a798732f34d434a7127f0d25a76ad2d46f514
HanSon <h@hanc.cc>
2017-02-17 19:10:43 +0800

    modify the vue instance for sidebar

--- src/publishable/resources/views/vendor/speedy/partials/sidebar.blade.php
+++ src/publishable/resources/views/vendor/speedy/partials/sidebar.blade.php
@@ -5,12 +5,12 @@
     </div>
     <div class="sidebar-body">
         <ul>
-            <template v-for="(menu, key) in sidebar.menus">
+            <template v-for="(menu, key) in menus">
                 <li class="first-menu-li">
                     <a v-bind:class="[isCurrentUrl(menu.url) ? 'active' : '', 'first-menu']" v-on:click="toggleMenu(key)" v-bind:id="'menu-' + key" v-bind:href="menu.url ? menu.url : 'javascript:;'">@{{ menu.display }}
                         <span v-if="menu.sub" class="glyphicon glyphicon-chevron-down"></span>
                     </a>
-                    <ul v-if="menu.sub" v-bind:id="'sub-menu-' + key" class="sub-menu" v-bind:style="{display: sidebar.active == key ? 'block' : 'none'}">
+                    <ul v-if="menu.sub" v-bind:id="'sub-menu-' + key" class="sub-menu" v-bind:style="{display: active == key ? 'block' : 'none'}">
                         <template v-for="sub in menu.sub">
                             <li class="second-menu-li"><a v-bind:href="sub.url" v-bind:target="sub.target ? sub.target : ''">@{{ sub.display }}</a></li>
                         </template>
